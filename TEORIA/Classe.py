57a5e2bcca7afb26a330983291ab2816ff8e39e1
Matheus Leandro <matheus.leandroyt@gmail.com>
2022-03-30 15:46:28 -0300

    atributo de classe

--- TEORIA/Classe.py
+++ TEORIA/Classe.py
@@ -1,27 +1,10 @@
 class Pessoa:
-    def __init__(self, *filhos, nome=None, idade=32): #MÉTODO CONSTRUTOR/ESPECIAL
-        self.nome = nome
-        self.idade = idade #ATRIBUTO DE DADOS
-        self.filhos = list(filhos) #ATRIBUTO COMPLEXO
+    olhos = 2 # atributo de classe
 
-    def cumprimentar(self): #método
-        return 'Olá'
-
-
-if __name__ == '__main__':  
-    matheus = Pessoa('Matheus', 'Leandro', 'Victor', nome = 'Theus') #OBJETO  #INSTÂNCIA DA CLASSE
-    matheus.esposa = 10 #ATRIBUTO DINÂMICO    
-
-
-    for filho in matheus.filhos:
-        print(filho)
-
-'''
-class Pessoa:
     def __init__(self, idade=None, nome='', *filhos):
-        self.idade = idade
+        self.idade = idade #atributo de dados
         self.nome = nome
-        self.filhos = list(filhos)
+        self.filhos = list(filhos) #atributo complexo
 
 if __name__ == ('__main__'):
     victor = Pessoa(nome='Victor', idade=5) #Filho de Matheus
@@ -29,11 +12,11 @@ if __name__ == ('__main__'):
     matheus = Pessoa(23, 'Matheus', victor, marcelo) #Pai de Victor e Marcelo
     victor.filhos = 'Victorzinho JR'
     matheus.neto = 'Victorzinho JR' #Atributo dinâmico -> atributo criado para o objeto "matheus" após o método construtor
+    
     for filho in matheus.filhos:
         print(filho.nome, filho.idade, filho.filhos)
     print(matheus.neto)
-
     print(victor.__dict__)
     print(marcelo.__dict__)
     print(matheus.__dict__)
-'''
+    print(matheus.olhos) # imprimindo a minha instância de atributo de classe
