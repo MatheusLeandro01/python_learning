b894dbef5a28a12a4c641a5403eb2f54b21fae40
Matheus Leandro <matheus.leandroyt@gmail.com>
2022-04-18 21:03:13 -0300

    Implementando as classes 'Direcao e Motor'

--- exercicio_composicao_classe/carro.py
+++ exercicio_composicao_classe/carro.py
@@ -87,21 +87,35 @@ O       L
     'Oeste'
 '''
 
+NORTE='Norte'
+SUL='Sul'
+LESTE='Leste'
+OESTE='Oeste'
+
+class Direcao:
+    rotacao_a_direita_dct = {
+        NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE
+        }
+    rotacao_a_esquerda_dct = {
+        NORTE: OESTE, OESTE: SUL, SUL: LESTE, LESTE: NORTE
+        }
+
+    def __init__(self):
+        self.valor = NORTE
+
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
 
 class Motor:
-    def __init__(self, velocidade=0):
-        self.velocidade = velocidade        
+    def __init__(self):
+        self.velocidade = 0
     
     def acelerar(self):
-        self.velocidade += 1        
+        self.velocidade += 1
 
     def frear(self):
         self.velocidade -= 2
-
-class Direcao:
-    def __init__(self, valor=['Norte', 'Sul', 'Leste', 'Oeste']):
-        self.valor = valor[0]
-
-    def girar_a_direita(self):
-        for cont in range(1):
-            self.valor = self.valor[cont+1]
+        self.velocidade = max(0, self.velocidade)
