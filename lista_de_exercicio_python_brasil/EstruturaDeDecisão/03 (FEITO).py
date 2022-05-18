ffd5e2b56d4b4f2c30026367203226f3415480c9
Matheus Leandro <matheus.leandroyt@gmail.com>
2022-05-18 08:49:43 -0300

    Comentário sobre (.upper())

--- lista_de_exercicio_python_brasil/EstruturaDeDecisão/03 (FEITO).py
+++ lista_de_exercicio_python_brasil/EstruturaDeDecisão/03 (FEITO).py
@@ -1,6 +1,9 @@
-#Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+'''
+#Faça um Programa que verifique se uma letra digitada é "F" ou "M".
+Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+'''
 
-letra = str(input('Informe "F" para feminino ou "M" para masculino: ')).upper()
+letra = str(input('Informe "F" para feminino ou "M" para masculino: ')).upper() #para transformar todas as letras em maiúsculo
 
 if letra == 'F':
     print('Feminino!')
